7e0dbfe1dc1be5f7136fba5b4d9c227673ea4924
Dipen <kalsidipen@gmail.com>
2023-01-23 02:14:22 +0530

    Initial commit

---
+++ app/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs';
+import {BsInstagram} from 'react-icons/bs';
+import {BsStackOverflow} from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
+import {BsGithub} from 'react-icons/bs';
+import {SiLeetcode} from 'react-icons/si'
+const Footer = () => {
+  return (
+    <div className='bg-[#0c0303]  flex justify-between items-center py-4 px-5'>
+      <p className='text-yellow-300 font-bold' style={{fontFamily:"'Roboto', sans-serif"}}>Copyright &copy; Dipen Kalsi , 2023. All rights reserved.</p>
+      <div className='text-yellow-300 flex space-x-6'>
+        <a href="https://github.com/dipenkalsi" target="_blank">
+          <BsGithub size="25px"/>
+        </a>
+        <a href="https://www.linkedin.com/in/dipen-kalsi-4448b5205/" target="_blank">
+          <BsLinkedin size="25px"/>
+        </a>
+        <a href="https://www.instagram.com/_dipen02/" target="_blank">
+          <BsInstagram size="25px"/>
+        </a>
+        <a href="https://twitter.com/DipenKalsi" target="_blank">
+          <BsTwitter size="25px"/>
+        </a>
+        <a href="https://stackoverflow.com/users/20255900/dipen-kalsi" target="_blank">
+          <BsStackOverflow size="25px"/>
+        </a>
+        <a href="https://leetcode.com/kalsidipen/" target="_blank">
+          <SiLeetcode size="25px"/>
+        </a>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Footer
